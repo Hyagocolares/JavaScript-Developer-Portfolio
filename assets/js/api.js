bd9aaf3938ba24b07c47979919728f10a7e95417
Hyago Colares <58047846+Hyagocolares@users.noreply.github.com>
2022-09-08 01:29:45 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Hyagocolares/JavaScript-Developer-Portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Hyagocolares/JavaScript-Developer-Portfolio/blob/master/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
